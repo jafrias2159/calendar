4bc5cd2f42fee81fe02ea4877aa412c83b14d1e5
Jorge Frias <jorge.frias@orioninc.com>
2023-02-20 09:25:44 -0600

    Adding events to calendar

---
+++ src/calendar/pages/CalendarPage.jsx
@@ -0,0 +1,69 @@
+import { Navbar } from '../components/Navbar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { Calendar } from 'react-big-calendar';
+import { addHours } from 'date-fns';
+import { localizer } from '../helpers/calendarLocalizer';
+import { CalendarEvent } from '../components/CalendarEvent';
+import { useState } from 'react';
+
+const events = [
+  {
+    title: 'Cumpleaños del Jefe',
+    notes: 'Hay que comprar el pastel',
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: '#fafafa',
+    user: {
+      _id: '123',
+      name: 'Jorge',
+    },
+  },
+];
+export const CalendarPage = () => {
+  const [view, setView] = useState(localStorage.getItem('lastView') || 'week');
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    // console.log({ event, start, end, isSelected });
+
+    const style = {
+      backgroundColor: 'orange',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white',
+    };
+
+    return { style };
+  };
+
+  const onDoubleClickHandler = (event) => {
+    console.log('onDoubleClickHandler', event);
+  };
+  const onSelectEventHandler = (event) => {
+    console.log('onSelectEventHandler', event);
+  };
+
+  const onviewChangeEventHandler = (event) => {
+    console.log('onviewChangeEventHandler', event);
+    localStorage.setItem('lastView', event);
+  };
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <div>
+        <Calendar
+          defaultView={view}
+          localizer={localizer}
+          events={events}
+          startAccessor='start'
+          endAccessor='end'
+          style={{ height: 'calc(100vh - 80px)' }}
+          eventPropGetter={eventStyleGetter}
+          components={{ event: CalendarEvent }}
+          onDoubleClickEvent={onDoubleClickHandler}
+          onSelectEvent={onSelectEventHandler}
+          onView={onviewChangeEventHandler}
+        />
+      </div>
+    </>
+  );
+};
